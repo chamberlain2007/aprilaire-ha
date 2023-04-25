e58b9261e04e5b6865f32126dab4d966e10dd21e
Matthew FitzGerald-Chamberlain <mattfitzgeraldchamberlain@proton.me>
2023-04-24 21:14:06 -0500

    Release 0.5.4 (#15)
    * Create tests.yml
    
    * Add coverage and update requirements.txt
    
    * Update tests.yml
    
    * Update tests.yml
    
    * Add unit tests (#13)
    
    * Add unit tests for coordinator
    
    * Add init tests
    
    * Add base entity tests
    
    * Add tests for sensors
    
    * Skip coverage for stop
    
    * Add tests for binary sensor
    
    * Test action
    
    * Add homeassistant dependency
    
    * Lock homeassistant version and add pyaprilaire dependency
    
    * Fix test in newer HA versions
    
    * Test action
    
    * Test action
    
    * Initial tests for climate
    
    * Complete initial tests for climate
    
    * Add unit tests for config flow
    
    * Update logging in tests
    
    * Fix signature
    
    * Skip noncovered lines
    
    * Bump version 0.5.3 -> 0.5.4rc0
    
    * Bump version 0.5.4rc0 -> 0.5.4

--- custom_components/aprilaire/coordinator.py
+++ custom_components/aprilaire/coordinator.py
@@ -3,16 +3,18 @@
 from __future__ import annotations
 
 import asyncio
-import logging
 
 from collections.abc import Awaitable, Callable
+from logging import Logger
 from typing import Any
 
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.helpers import device_registry as dr
+import homeassistant.helpers.device_registry
 
+import pyaprilaire.client
 from pyaprilaire.client import AprilaireClient
 from pyaprilaire.const import FunctionalDomain, MODELS
 
@@ -21,27 +23,27 @@ from .const import DOMAIN, LOG_NAME
 RECONNECT_INTERVAL = 60 * 60
 RETRY_CONNECTION_INTERVAL = 10
 
-_LOGGER = logging.getLogger(LOG_NAME)
-
 
 class AprilaireCoordinator(DataUpdateCoordinator):
     """Coordinator for interacting with the thermostat"""
 
-    def __init__(self, hass: HomeAssistant, host: str, port: int) -> None:
+    def __init__(
+        self, hass: HomeAssistant, host: str, port: int, logger: Logger
+    ) -> None:
         """Initialize the coordinator"""
         super().__init__(
             hass,
-            _LOGGER,
+            logger,
             name=DOMAIN,
         )
 
         self.data: dict[str, Any] = {}
 
-        self.client = AprilaireClient(
+        self.client = pyaprilaire.client.AprilaireClient(
             host,
             port,
             self.async_set_updated_data,
-            _LOGGER,
+            self.logger,
             RECONNECT_INTERVAL,
             RETRY_CONNECTION_INTERVAL,
         )
@@ -61,7 +63,7 @@ class AprilaireCoordinator(DataUpdateCoordinator):
             and new_device_info is not None
             and old_device_info != new_device_info
         ):
-            device_registry = dr.async_get(self.hass)
+            device_registry = homeassistant.helpers.device_registry.async_get(self.hass)
 
             device = device_registry.async_get_device(old_device_info["identifiers"])
 
@@ -80,38 +82,31 @@ class AprilaireCoordinator(DataUpdateCoordinator):
         """Stop listening for data"""
         self.client.stop_listen()
 
-    def wait_for_ready(self, ready_callback: Callable[[bool], Awaitable[None]]):
-        """Makes requests needed for startup and waits for necessary data to be retrieved"""
+    async def wait_for_ready(self, ready_callback: Callable[[bool], Awaitable[None]]):
+        if not self.data or "mac_address" not in self.data:
+            data = await self.client.wait_for_response(
+                FunctionalDomain.IDENTIFICATION, 2, 30
+            )
 
-        async def _run():
-            if not self.data or "mac_address" not in self.data:
-                data = await self.client.wait_for_response(
-                    FunctionalDomain.IDENTIFICATION, 2, 30
-                )
+            if not data or "mac_address" not in data:
+                self.logger.error("Missing MAC address, cannot create unique ID")
+                await ready_callback(False)
 
-                if not data or "mac_address" not in data:
-                    _LOGGER.error("Missing MAC address, cannot create unique ID")
-                    await ready_callback(False)
+                return
 
-                    return
+        if not self.data or "name" not in self.data:
+            await self.client.wait_for_response(FunctionalDomain.IDENTIFICATION, 4, 30)
 
-            if not self.data or "name" not in self.data:
-                await self.client.wait_for_response(
-                    FunctionalDomain.IDENTIFICATION, 4, 30
-                )
+        if not self.data or "thermostat_modes" not in self.data:
+            await self.client.wait_for_response(FunctionalDomain.CONTROL, 7, 30)
 
-            if not self.data or "thermostat_modes" not in self.data:
-                await self.client.wait_for_response(FunctionalDomain.CONTROL, 7, 30)
+        if (
+            not self.data
+            or "indoor_temperature_controlling_sensor_status" not in self.data
+        ):
+            await self.client.wait_for_response(FunctionalDomain.SENSORS, 2, 30)
 
-            if (
-                not self.data
-                or "indoor_temperature_controlling_sensor_status" not in self.data
-            ):
-                await self.client.wait_for_response(FunctionalDomain.SENSORS, 2, 30)
-
-            await ready_callback(True)
-
-        asyncio.ensure_future(_run())
+        await ready_callback(True)
 
     @property
     def device_name(self) -> str:
@@ -126,6 +121,14 @@ class AprilaireCoordinator(DataUpdateCoordinator):
             return "Aprilaire"
 
         return name
+
+    def get_hw_version(self, data: dict[str, Any]) -> str:
+        hardware_revision = data.get("hardware_revision")
+        if hardware_revision is not None:
+            if hardware_revision > ord("A"):
+                return f"Rev. {chr(hardware_revision)}"
+            else:
+                return str(hardware_revision)
 
     @property
     def device_info(self) -> DeviceInfo:
@@ -152,12 +155,7 @@ class AprilaireCoordinator(DataUpdateCoordinator):
                 else f"Unknown ({model_number})"
             )
 
-        hardware_revision = data.get("hardware_revision")
-        if hardware_revision is not None:
-            if hardware_revision > ord("A"):
-                device_info["hw_version"] = f"Rev. {chr(hardware_revision)}"
-            else:
-                device_info["hw_version"] = hardware_revision
+        device_info["hw_version"] = self.get_hw_version(data)
 
         firmware_major_revision = data.get("firmware_major_revision")
         firmware_minor_revision = data.get("firmware_minor_revision")
---
+++ tests/test_binary_sensor.py
@@ -0,0 +1,62 @@
+from custom_components.aprilaire.coordinator import AprilaireCoordinator
+from custom_components.aprilaire.const import DOMAIN
+from custom_components.aprilaire.binary_sensor import (
+    async_setup_entry,
+    AprilaireFanStatusSensor,
+)
+
+from homeassistant.config_entries import ConfigEntry, ConfigEntries
+from homeassistant.core import Config, HomeAssistant, EventBus
+from homeassistant.util import uuid as uuid_util
+
+import unittest
+from unittest.mock import AsyncMock, Mock
+
+
+class Test_Binary_Sensor(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        self.coordinator_mock = AsyncMock(AprilaireCoordinator)
+        self.coordinator_mock.data = {}
+
+        self.entry_id = uuid_util.random_uuid_hex()
+
+        self.hass_mock = AsyncMock(HomeAssistant)
+        self.hass_mock.data = {DOMAIN: {self.entry_id: self.coordinator_mock}}
+        self.hass_mock.config_entries = AsyncMock(ConfigEntries)
+        self.hass_mock.bus = AsyncMock(EventBus)
+        self.hass_mock.config = Mock(Config)
+
+        self.config_entry_mock = AsyncMock(ConfigEntry)
+        self.config_entry_mock.data = {"host": "test123", "port": 123}
+        self.config_entry_mock.entry_id = self.entry_id
+
+    async def test_fan_status_sensor(self):
+        self.coordinator_mock.data = {
+            "fan_status": 0,
+        }
+
+        async_add_entities_mock = Mock()
+
+        await async_setup_entry(
+            self.hass_mock, self.config_entry_mock, async_add_entities_mock
+        )
+
+        sensors_list = async_add_entities_mock.call_args_list[0][0]
+
+        self.assertEqual(len(sensors_list), 1)
+
+        sensor = sensors_list[0][0]
+
+        self.assertIsInstance(sensor, AprilaireFanStatusSensor)
+
+        sensor._available = True
+
+        self.assertTrue(sensor.available)
+        self.assertEqual(sensor.entity_name, "Fan")
+        self.assertFalse(sensor.is_on)
+
+        self.coordinator_mock.data = {
+            "fan_status": 1,
+        }
+
+        self.assertTrue(sensor.is_on)

4e23d6ed7085e202ad8574fc1300b7ff6c975af1
Matthew FitzGerald-Chamberlain <chamberlain2007@gmail.com>
2023-06-20 21:19:34 -0500

    Implement pytest for entity

--- tests/test_entity.py
+++ tests/test_entity.py
@@ -1,135 +1,156 @@
+# pylint: skip-file
+
 import logging
-import unittest
 from unittest.mock import AsyncMock, Mock, patch
 
+import pytest
 from homeassistant.helpers.entity import DeviceInfo
 
-from custom_components.aprilaire.const import LOG_NAME
 from custom_components.aprilaire.coordinator import AprilaireCoordinator
 from custom_components.aprilaire.entity import BaseAprilaireEntity
 
-_LOGGER = logging.getLogger(LOG_NAME)
+
+@pytest.fixture
+def logger():
+    logger = logging.getLogger()
+    logger.propagate = False
+
+    return logger
 
 
-class Test_Entity(unittest.IsolatedAsyncioTestCase):
-    def setUp(self):
-        self.coordinator_mock = AsyncMock(AprilaireCoordinator)
-        self.coordinator_mock.data = {}
-        self.coordinator_mock.logger = _LOGGER
+@pytest.fixture
+def coordinator(logger: logging.Logger) -> AprilaireCoordinator:
+    coordinator_mock = AsyncMock(AprilaireCoordinator)
+    coordinator_mock.data = {}
+    coordinator_mock.logger = logger
 
-    async def test_available_on_init(self):
-        update_available_mock = Mock()
-        with patch(
-            "custom_components.aprilaire.entity.BaseAprilaireEntity._update_available",
-            new=update_available_mock,
-        ):
-            entity = BaseAprilaireEntity(self.coordinator_mock)
-        update_available_mock.assert_called_once()
+    return coordinator_mock
 
-    async def test_handle_coordinator_update(self):
-        update_available_mock = Mock()
-        async_write_ha_state_mock = Mock()
 
-        with patch(
-            "custom_components.aprilaire.entity.BaseAprilaireEntity._update_available",
-            new=update_available_mock,
-        ), patch(
-            "homeassistant.helpers.entity.Entity.async_write_ha_state",
-            new=async_write_ha_state_mock,
-        ):
-            entity = BaseAprilaireEntity(self.coordinator_mock)
-            entity._handle_coordinator_update()
+async def test_available_on_init(coordinator: AprilaireCoordinator):
+    update_available_mock = Mock()
+    with patch(
+        "custom_components.aprilaire.entity.BaseAprilaireEntity._update_available",
+        new=update_available_mock,
+    ):
+        entity = BaseAprilaireEntity(coordinator)
+    update_available_mock.assert_called_once()
 
-        self.assertEqual(update_available_mock.call_count, 2)
-        async_write_ha_state_mock.assert_called_once()
 
-    async def test_update_available_stopped(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
+async def test_handle_coordinator_update(coordinator: AprilaireCoordinator):
+    update_available_mock = Mock()
+    async_write_ha_state_mock = Mock()
 
-        self.coordinator_mock.data["stopped"] = True
-        entity._update_available()
+    with patch(
+        "custom_components.aprilaire.entity.BaseAprilaireEntity._update_available",
+        new=update_available_mock,
+    ), patch(
+        "homeassistant.helpers.entity.Entity.async_write_ha_state",
+        new=async_write_ha_state_mock,
+    ):
+        entity = BaseAprilaireEntity(coordinator)
+        entity._handle_coordinator_update()
 
-        self.assertFalse(entity._attr_available)
-        self.assertFalse(entity.available)
+    assert update_available_mock.call_count == 2
 
-    async def test_update_available_no_mac(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
+    async_write_ha_state_mock.assert_called_once()
 
-        self.coordinator_mock.data["connected"] = True
-        self.coordinator_mock.data["stopped"] = False
-        self.coordinator_mock.data["mac_address"] = None
-        entity._update_available()
 
-        self.assertFalse(entity._attr_available)
-        self.assertFalse(entity.available)
+async def test_update_available_stopped(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
 
-    async def test_update_available_connected_not_stopped(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
+    coordinator.data["stopped"] = True
+    entity._update_available()
 
-        self.coordinator_mock.data["connected"] = True
-        self.coordinator_mock.data["stopped"] = False
-        self.coordinator_mock.data["mac_address"] = "1:2:3:4:5:6"
-        entity._update_available()
+    assert entity._attr_available is False
+    assert entity.available is False
 
-        self.assertTrue(entity._attr_available)
-        self.assertTrue(entity.available)
 
-    async def test_update_available_reconnecting_not_stopped(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
+async def test_update_available_no_mac(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
 
-        self.coordinator_mock.data["connected"] = False
-        self.coordinator_mock.data["reconnecting"] = True
-        self.coordinator_mock.data["stopped"] = False
-        self.coordinator_mock.data["mac_address"] = "1:2:3:4:5:6"
-        entity._update_available()
+    coordinator.data["connected"] = True
+    coordinator.data["stopped"] = False
+    coordinator.data["mac_address"] = None
+    entity._update_available()
 
-        self.assertTrue(entity._attr_available)
-        self.assertTrue(entity.available)
+    assert entity._attr_available is False
+    assert entity.available is False
 
-    def test_should_poll(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
 
-        self.assertFalse(entity.should_poll)
+async def test_update_available_connected_not_stopped(
+    coordinator: AprilaireCoordinator,
+):
+    entity = BaseAprilaireEntity(coordinator)
 
-    def test_unique_id(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
+    coordinator.data["connected"] = True
+    coordinator.data["stopped"] = False
+    coordinator.data["mac_address"] = "1:2:3:4:5:6"
+    entity._update_available()
 
-        self.coordinator_mock.data["mac_address"] = "1:2:3:4:5:6"
+    assert entity._attr_available is True
+    assert entity.available is True
 
-        with patch(
-            "custom_components.aprilaire.entity.BaseAprilaireEntity.entity_name",
-            new="Test Entity",
-        ):
-            self.assertEqual(entity.unique_id, "1_2_3_4_5_6_test_entity")
 
-    def test_name(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
-        self.coordinator_mock.device_name = "Aprilaire"
+async def test_update_available_reconnecting_not_stopped(
+    coordinator: AprilaireCoordinator,
+):
+    entity = BaseAprilaireEntity(coordinator)
 
-        with patch(
-            "custom_components.aprilaire.entity.BaseAprilaireEntity.entity_name",
-            new="Test Entity",
-        ):
-            self.assertEqual(entity.name, "Aprilaire Test Entity")
+    coordinator.data["connected"] = False
+    coordinator.data["reconnecting"] = True
+    coordinator.data["stopped"] = False
+    coordinator.data["mac_address"] = "1:2:3:4:5:6"
+    entity._update_available()
 
-    def test_base_entity_name(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
-        self.assertEqual("", entity.entity_name)
+    assert entity._attr_available is True
+    assert entity.available is True
 
-    def test_extra_state_attributes(self):
-        entity = BaseAprilaireEntity(self.coordinator_mock)
-        self.coordinator_mock.device_name = "Aprilaire"
-        self.coordinator_mock.data["location"] = "Test Location"
 
-        self.assertDictEqual(
-            entity.extra_state_attributes,
-            {"device_name": "Aprilaire", "device_location": "Test Location"},
-        )
+def test_should_poll(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
 
-    def test_device_info(self):
-        self.coordinator_mock.device_info = DeviceInfo()
+    assert entity.should_poll is False
 
-        entity = BaseAprilaireEntity(self.coordinator_mock)
-        device_info = entity.device_info
 
-        self.assertEqual(device_info, self.coordinator_mock.device_info)
+def test_unique_id(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
+
+    coordinator.data["mac_address"] = "1:2:3:4:5:6"
+
+    with patch(
+        "custom_components.aprilaire.entity.BaseAprilaireEntity.entity_name",
+        new="Test Entity",
+    ):
+        assert entity.unique_id == "1_2_3_4_5_6_test_entity"
+
+
+def test_name(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
+    coordinator.device_name = "Aprilaire"
+
+    with patch(
+        "custom_components.aprilaire.entity.BaseAprilaireEntity.entity_name",
+        new="Test Entity",
+    ):
+        assert entity.name == "Aprilaire Test Entity"
+
+
+def test_extra_state_attributes(coordinator: AprilaireCoordinator):
+    entity = BaseAprilaireEntity(coordinator)
+    coordinator.device_name = "Aprilaire"
+    coordinator.data["location"] = "Test Location"
+
+    assert entity.extra_state_attributes == {
+        "device_name": "Aprilaire",
+        "device_location": "Test Location",
+    }
+
+
+def test_device_info(coordinator: AprilaireCoordinator):
+    coordinator.device_info = DeviceInfo()
+
+    entity = BaseAprilaireEntity(coordinator)
+    device_info = entity.device_info
+
+    assert device_info == coordinator.device_info
